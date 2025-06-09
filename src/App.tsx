cdbc214dcc59d950edb88597882e2fe720cc6f95
codehass <hassan.elouardy06@gmail.com>
2025-06-09 02:34:45 +0100

    add transition styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import './App.css'
 import SkipOptions from './components/SkipOptions';
-import { ThemeColors } from './components/ThemeColors';
-import { ThemeProvider } from './components/ThemeContext';
+
 
 function App() {
 
   return (
-    <ThemeProvider>
-      <ThemeColors>
-        <SkipOptions />
-      </ThemeColors>
-    </ThemeProvider>
+    <SkipOptions />
   )
 }
 
